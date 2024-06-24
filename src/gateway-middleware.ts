4d1181410019a51c997aaacb13e1349d4f3cdbeb
Harsh Bhama <harshbhama96@gmail.com>
2024-06-24 16:11:22 +0530

    add changes for gateway-middleware

--- src/gateway-middleware.ts
+++ src/gateway-middleware.ts
@@ -18,20 +18,21 @@ export function verifyGatewayRequest(
   res: Response,
   next: NextFunction
 ): void {
+  console.log("req.headers", req.headers);
   if(!req.headers?.gatewaytoken){
-    throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway');
+    throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway-headers not present');
   }
-  const token: string = req.headers?.gatewayToken as string;
+  const token: string = req.headers?.gatewaytoken as string;
   if(!token){
-    throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway');
+    throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway-token not present');
   }
   try{
     const payload: {id: string, iat: number} = JWT.verify(token, '8f81e5ee261bd5a03f14b6b5316815ff') as {id: string, iat: number} ;
     if(!token.includes(payload?.id)){
-      throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway');
+      throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway-payload-id not present');
     }
   }catch(err){
-    throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway');
+    throw new NotAuthorizedError('Invalid request', 'verifyGatewayRequest() method: Request not coming from api gateway-cartch error');
 
   }
   next();
